8c10153a9d848c88872201da989d3a0641fa6128
Dominik del Bondio <dominik.del.bondio@bitextender.com>
2016-11-10 11:43:04 +0100

    update to use sha256 checksums

--- nginx_ensite.rb
+++ nginx_ensite.rb
@@ -7,16 +7,16 @@ class NginxEnsite < Formula
   homepage 'https://github.com/perusio/nginx_ensite'
   url 'https://github.com/perusio/nginx_ensite/tarball/77bf95750560b0d6cc82047f5908b1eae4f0e314'
   version '20120225'
-  sha1 '68c204995db323ae4e5f39ec514f394e13207390'
+  sha256 '8b82f64770b6eac90d45f6f0b6039cbd2f43360eae8d2c1942f7bbc97fbb0303'
 
   depends_on 'nginx'
-  
+
   def install
     nginx_etc = etc/'nginx'
-    inreplace 'nginx_ensite' do |s| 
+    inreplace 'nginx_ensite' do |s|
       s.sub! 'NGINX_CONF_DIR=/etc/nginx', "NGINX_CONF_DIR=#{nginx_etc}"
     end
-    
+
     bin.install('nginx_ensite', 'nginx_dissite')
     man.mkpath
     man8.install('man/nginx_ensite.8', 'man/nginx_dissite.8')
